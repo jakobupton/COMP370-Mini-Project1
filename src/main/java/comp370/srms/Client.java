54ab132aa2bd4c769e01975c81d198cd82dc313a
Jakob Hobek <hobekjakob@gmail.com>
2026-02-11 20:55:11 -0800

    Client reconnection attempt
    Client will now attempt to reconnect to the primary server up to 5 times before exiting.
    
    Client now sends PROCESS requests to the primary every second.

--- src/main/java/comp370/srms/Client.java
+++ src/main/java/comp370/srms/Client.java
@@ -1,50 +1,78 @@
 package comp370.srms;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 class Client {
     public static String currentPrimary = "";
+    private static MessageSocket Primary;
+    private static String PrimaryIP;
+    private static int PrimaryPort;
+    private static int MaxRetryAttempts = 5;
+    private static boolean connected = false;
     public static void main(String[] args) {
+        int attempt = 0;
+        while (attempt < MaxRetryAttempts) {
+            while (connected) {
+                try (MessageSocket Primary = MessageSocket.connect(PrimaryIP, PrimaryPort)) {
+                    attempt = 0;
+                    while (!Primary.isClosed()) {
+                        Primary.send(MessageSerializer.serializeProcess());
+                        MessageSerializer.Message ProcessingMessage = Primary.readMessage();
+                        if (ProcessingMessage == null) {
+                            continue;
+                        }
+                        if (ProcessingMessage.type() != MessageSerializer.Type.PROCESSING) {
+                            Primary.send(MessageSerializer.serializeError("Expected PROCESSING, got " + ProcessingMessage.type()));
+                        }
+                        System.out.println("Server is processing!");
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+                    connected = false;
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+
+            try {
+                GetPrimaryAddress();
+            } catch (Exception e) {
+                attempt++;
+                System.out.println("Attempt " + attempt + " failed: " + e.getMessage());
+                if (attempt >= MaxRetryAttempts) {
+                    System.out.println("Failed to connect to Primary after " + attempt + " attempts.");
+                    return;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ignored) {}
+            }
+
+        }
+    }
+
+    private static void GetPrimaryAddress() throws IOException {
         String MonitorIP = "127.0.0.1";
         int MonitorPort = 3001;
-        String primaryRemoteAddr;
-
-        try (Socket MonitorConn = new Socket(MonitorIP, MonitorPort)){
+        try (Socket MonitorConn = new Socket(MonitorIP, MonitorPort)) {
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(MonitorConn.getInputStream())
             );
             PrintWriter out = new PrintWriter(
                     MonitorConn.getOutputStream(), true
             );
-            String req = MessageSerializer.serializeGetPrimary();
-            out.println(req);
+            String request = MessageSerializer.serializeGetPrimary();
+            out.println(request);
             String response = in.readLine();
-            MessageSerializer.Message msg = MessageSerializer.deserialize(response);
-            primaryRemoteAddr = msg.detail();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-
-        String primaryIp = primaryRemoteAddr.split(":")[0];
-        primaryIp = primaryIp.replace("/", "");
-        int primaryPort = Integer.parseInt(primaryRemoteAddr.split(":")[1]);
-        try (MessageSocket msg = MessageSocket.connect(primaryIp, primaryPort)) {
-            msg.send(MessageSerializer.serializeProcess());
-            MessageSerializer.Message processingMessage = msg.readMessage();
-            if (processingMessage == null) {
-                return;
-            }
-            if (processingMessage.type() != MessageSerializer.Type.PROCESSING) {
-                msg.send(MessageSerializer.serializeError("Expected PROCESSING, got " + processingMessage.type()));
-            }
-            System.out.println("Server is processing!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return;
+            MessageSerializer.Message message = MessageSerializer.deserialize(response);
+            String primaryRemoteAddr = message.detail();
+            String[] parts = primaryRemoteAddr.split(":");
+            PrimaryIP = parts[0].replace("/", "");
+            PrimaryPort = Integer.parseInt(parts[1]);
+            connected = true;
         }
     }
 }
